3472aaca9cb759525801465ad96d8734cc33ee70
Dominoman <olaca@freemail.hu>
2025-12-26 21:01:08 +0100

    Gunicorn web service

--- run-me.sh
+++ run-me.sh
@@ -22,3 +22,20 @@ crontab -l | grep -v "LongWeekend" > newcron
 echo "30 */6 * * * cd $currentpath && /home/$username/.local/bin/uv run flask scan >> $currentpath/log.log 2>&1" >> newcron
 crontab newcron
 rm newcron
+
+
+servicename="LongWeekend-web"
+
+if [ ! -f $servicename.service ] ; then
+  cp $servicename.service.template $servicename.service
+  sed -i "s|%currentpath%|$currentpath|g; s|%username%|$username|g; s|%groupname%|$groupname|g" $servicename.service
+fi
+
+if [ ! -f /etc/systemd/system/$servicename.service ] ; then
+  sudo ln -s "$currentpath/$servicename.service" /etc/systemd/system/$servicename.service
+fi
+
+# Restart gunicorn
+sudo systemctl daemon-reload
+sudo systemctl enable $servicename
+sudo systemctl restart $servicename
